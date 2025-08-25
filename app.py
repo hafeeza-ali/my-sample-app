8384613116b9e404a20e2ad45eb1c3166b0a052f
Hafeez <hafeeza470@gmail.com>
2025-08-25 11:21:31 +0530

    Test: Intentionally break the build

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # This is the simple application we want to test and "deploy".
 
-def add(a, b):
+'''def add(a, b):
     """This function adds two numbers."""
-    return a + b
+    return a + b'''
+def add(a, b):
+    return a + b + 1 # This is now incorrect
 
 def subtract(a, b):
     """This function subtracts two numbers."""
