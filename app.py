003e8b13a7a5d2e7c6140bc39dd0f2297eaac05c
Hafeez <hafeeza470@gmail.com>
2025-08-25 11:52:11 +0530

    Fix: Corrected the add function, build should now pass

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # This is the simple application we want to test and "deploy".
 
-'''def add(a, b):
-    """This function adds two numbers."""
-    return a + b'''
 def add(a, b):
+    """This function adds two numbers."""
+    return a + b
+'''def add(a, b):
     return a + b + 1 # This is now incorrect
+'''
 
 def subtract(a, b):
     """This function subtracts two numbers."""
