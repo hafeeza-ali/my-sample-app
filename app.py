f6bd316a7f494f8010c8301cf91dc77df667448f
Hafeez <hafeeza470@gmail.com>
2025-08-25 10:40:39 +0530

    Initial commit of sample application

---
+++ app.py
@@ -0,0 +1,9 @@
+# This is the simple application we want to test and "deploy".
+
+def add(a, b):
+    """This function adds two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """This function subtracts two numbers."""
+    return a - b
---
+++ test_app.py
@@ -0,0 +1,15 @@
+# This file contains the tests for our application.
+
+import pytest
+from app import add, subtract
+
+def test_add():
+    """Tests the add function."""
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-5, -5) == -10
+
+def test_subtract():
+    """Tests the subtract function."""
+    assert subtract(10, 5) == 5
+    assert subtract(-1, 1) == -2
